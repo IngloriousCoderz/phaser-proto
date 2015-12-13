ad4e78ded641f182ad56472490a16f9124e54669
IceOnFire <antony.mistretta@gmail.com>
2015-12-13 18:17:54 +0100

    Change game name

---
+++ src/farmgame.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+    var game, animalNames, cursors, animals, score, scoreText;
+    game = new Phaser.Game(320, 480, Phaser.AUTO, '', {
+        preload: preload,
+        create: create,
+        update: update
+    });
+    animalNames = ['cow', 'sheep', 'horse', 'chicken'];
+    score = 0;
+    function preload() {
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('star', 'assets/star.png');
+        for (var i = 0; i < animalNames.length; i++) {
+            game.load.image(animalNames[i], 'assets/animals/' + animalNames[i] + '.png');
+        }
+    }
+    function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.add.sprite(0, 0, 'sky');
+        animals = game.add.group();
+        var hOffset = 16;
+        var vOffset = game.world.height - 64 - 16;
+        for (var i = 0; i < animalNames.length; i++) {
+            animals.create(8 + i * hOffset + i * 64, vOffset, animalNames[i]);
+        }
+        cursors = game.input.keyboard.createCursorKeys();
+        scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    }
+    function update() {
+    }
+    function collectStar(player, star) {
+        star.kill();
+        score += 10;
+        scoreText.text = 'score: ' + score;
+    }
+};
